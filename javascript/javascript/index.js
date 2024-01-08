e30c4f2df7c2bb3d30b81c5697858669ed7c5ecc
unknown <alekshodo@gmail.com>
2024-01-08 10:33:39 +0100

    Hodo

---
+++ javascript/javascript/index.js
@@ -0,0 +1,84 @@
+//let email = "briken@gjergj.com"
+//let result = email.includes("@")
+//console.log(result)
+
+
+//let names = ["leo","sildi","eni"]
+//console.log(names.includes("redi"))
+
+
+//let age = 25
+//console.log(age == 25)
+//console.log(age == 30)
+//console.log(age > 25)
+//console.log(age < 25)
+//console.log(age >= 25)
+//console.log(age <= 25)
+//console.log(age !=35)
+
+
+
+
+//let name = "shaban"
+//console.log(name == "shaban")
+//console.log(name == "Shaban")
+//console.log(name >"cimi")
+//console.log(name >"shabani")
+
+
+
+//let age = 25
+//console.log(age == 25)
+//console.log(age === "25")
+//console.log(age !=20)
+//console.log(age !="15")
+//console.log(age !=="25")
+
+
+//let score = '100'
+//console.log(score+1)
+//console.log(typeof score)
+
+
+
+//let result = String(50)
+//console.log(typeof result,result)
+
+
+
+let result = Boolenn(0)
+console.log(typeof result,result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
